4dbe79b72a1bff1a28905aa466b966a6cee6ce33
fakerplayers <134500046+lenta228@users.noreply.github.com>
2025-06-17 20:01:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,32 @@
-import telebot
-import os
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("7431943285:AAEsgkQ0Bu00kV6eWdsdJPqWsCkvjjk4w3U")  # токен из переменной окружения
+# Настройка логгирования
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
-bot = telebot.TeleBot(TOKEN)
+# Токен вашего бота
+TOKEN = "7431943285:AAEsgkQ0Bu00kV6eWdsdJPqWsCkvjjk4w3U"
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я живой!")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Отправляет сообщение при получении команды /start."""
+    user = update.effective_user
+    await update.message.reply_text(f"Привет, как дела?")
 
-print("✅ Бот запущен")
-bot.infinity_polling()  # бот начинает слушать Telegram
+def main() -> None:
+    """Запуск бота."""
+    # Создаем Application и передаем токен бота
+    application = Application.builder().token(TOKEN).build()
+
+    # Добавляем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
